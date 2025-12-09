57709cfd44299ee60c1f61079a4d11278244542a
novato-intlgnt <emilio.pallardel@gmail.com>
2025-12-09 03:16:21 -0500

    delete(https): Now not using https

--- main.py
+++ main.py
@@ -4,29 +4,7 @@ from src.dev.models import *  # importar todos los modelos
 # Crear la app con dependencias inyectadas (como en JS)
 app = create_app()
 
-# Si se ejecuta directamente
-# if __name__ == "__main__":
-#     import uvicorn
-#
-#     uvicorn.run("main:app", host="0.0.0.0", port=4000, reload=True)
-
 if __name__ == "__main__":
-    import os
-
     import uvicorn
 
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    print(BASE_DIR)
-    CERT_DIR = os.path.join(BASE_DIR, "certs")
-
-    SSL_CERT = os.path.join(CERT_DIR, "localhost+2.pem")
-    SSL_KEY = os.path.join(CERT_DIR, "localhost+2-key.pem")
-
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=4000,
-        reload=True,
-        ssl_certfile=SSL_CERT,
-        ssl_keyfile=SSL_KEY,
-    )
+    uvicorn.run("main:app", host="0.0.0.0", port=4000, reload=True)
